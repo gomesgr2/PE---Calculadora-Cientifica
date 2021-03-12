cdf9e0c3654a44d9ae6396304e02bd3e78f2725b
gomesgr2 <gabriel2011.time@hotmail.com>
2021-03-12 11:33:14 -0300

    first commit

--- main.c
+++ main.c
@@ -1,6 +1,25 @@
 #include<stdio.h>
+#define MAX_TAMANHO_DA_PALAVRA 40
+#define NUMERO_DE_OPERACOES 3
 
 int main(){
+
+    // Exemplos da lista de operações
+
+    char arr[NUMERO_DE_OPERACOES][MAX_TAMANHO_DA_PALAVRA] = { "Soma", "Subtracao", "Fatorial"};
+
+    // Exemplo da impressão inicial
+
+    printf("Bem vindo a UFABCalc\nDigite o numero para realizar as operacoes\n");
+
+
+    for(int i =0; i < NUMERO_DE_OPERACOES; i++  ){
+        printf("%d : %s\n",i, arr[i]);
+    }
+
+    // Entradas 
+
+
 
     
 
